1732863ad8a6dcfb996d7a1a532d944d379f8fd8
abdulsamii786 <154473245+abdulsamii786@users.noreply.github.com>
2024-02-27 22:31:48 +0500

    Add files via upload

---
+++ ASSIGNMENT 1/app.js
@@ -0,0 +1,47 @@
+// Q1:Write a script to greet your website visitor using JS alert box.
+
+alert("Hello World")
+
+// Q2:Write a script to display following message on your web page:
+
+alert("Error! Please enter a valid password")
+
+// Q3:Write a script to display following message on your web page: (Hint : Use line break)
+
+alert("Welcome to JS Land...\n Happy Coding!")
+
+// Q4:Write a script to display following messages in sequence:
+
+alert("Welcome to JS Land...")
+alert("Happy Coding!")
+
+
+// Q5:Generate the following message through browser’s developer console:
+
+console.log("Hello... I can run JS through my browser's console")
+
+
+// ========================using sweet alert================================
+
+
+// Q1:Write a script to greet your website visitor using JS alert box.
+
+swal("Hello World")
+
+// Q2:Write a script to display following message on your web page:
+
+swal("Error! Please enter a valid password")
+
+// // Q3:Write a script to display following message on your web page: (Hint : Use line break)
+
+swal("Welcome to JS Land...\n Happy Coding!")
+
+// // Q4:Write a script to display following messages in sequence:
+
+swal("Welcome to JS Land...")
+swal("Happy Coding!")
+
+
+// Q5:Generate the following message through browser’s developer console:
+
+console.log("Hello... I can run JS through my browser's console")
