36efb3de88754c6929a82c32924e2520ff5577ec
abdulsamii786 <154473245+abdulsamii786@users.noreply.github.com>
2024-03-07 23:51:10 +0500

    Add files via upload

---
+++ Calculator Practice/app.js
@@ -0,0 +1,38 @@
+// =============CALCULATOR=============
+
+// 'prompt' se pehly '+' isliye lagaya hai takay output numbers me aye string me na aye
+
+// pehlay variables banaye gaye hain
+
+let num1 = +prompt("Enter First Number");
+let num2 = +prompt("Enter Second Number");
+
+// =====addition======
+
+let sum = num1 + num2;
+
+document.write("The Sum Of Your Numbers is " + sum + "</br>");
+
+// =====Subtraction======
+
+let sub = num1 - num2;
+
+document.write("The Subtraction Of Your Numbers is " + sub + "</br>");
+
+// =====Multiplication======
+
+let multiply = num1 * num2;
+
+document.write("The Multiplication Of Your Numbers is " + multiply + "</br>");
+
+// =====Division======
+
+let div = num1 / num2;
+
+document.write("The Division Of Your Numbers is " + div + "</br>");
+
+// =====Remainders======
+
+let remainder = num1 % num2;
+
+document.write("The Remainders Of Your Numbers is " + remainder + "</br>");
