076913187970b6d180360f8845154f438f25b56a
abdulsamii786 <154473245+abdulsamii786@users.noreply.github.com>
2024-03-26 22:34:58 +0500

    Add files via upload

---
+++ ASSIGNMENT 5/app.js
@@ -0,0 +1,25 @@
+// =====================USER INPUT & CONDITIONAL STATEMENT=======================
+
+// Q1. Write a program to take “city” name as input from user. If user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+
+// let userCity = prompt("Enter Your City Name");
+// if (userCity.toLowerCase() === "karachi") {
+//   swal.fire("“Welcome to city of lights”");
+// } else {
+//   swal.fire("“Welcome to city”");
+// }
+
+// -------------------------------------
+
+// Q2. Write a program to take “gender” as input from user. If the user is male, give the message: Good Morning Sir. If the user is female, give the message: Good Morning Ma’am.
+
+// let gender = prompt("Enter Your Gender");
+
+// if (gender.toLowerCase() === "male") {
+//   swal.fire("Good Morning Sir");
+// } else if (gender.toLowerCase() === "female") {
+//   swal.fire("Good Morning Ma'am");
+// } else {
+//   swal.fire("Good Morning");
+// }
