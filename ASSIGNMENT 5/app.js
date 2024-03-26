36b517fee56e4754b3260e2ec5b5d06ebfdf0c93
abdulsamii786 <154473245+abdulsamii786@users.noreply.github.com>
2024-03-26 23:18:20 +0500

    Add files via upload

--- ASSIGNMENT 5/app.js
+++ ASSIGNMENT 5/app.js
@@ -23,3 +23,38 @@
 // } else {
 //   swal.fire("Good Morning");
 // }
+
+// ----------------------
+
+// Q3. Write a program to take input color of road traffic signal from the user & show the message according to this table:
+
+// let color = prompt(
+//   "Please write down the color you are observing at Traffic Signal!"
+// );
+
+// if (color.toLowerCase() === "red") {
+//   swal.fire("Must Stop!");
+// } else if (color.toLowerCase() === "yellow") {
+//   swal.fire("Ready To Move!");
+// } else if (color.toLowerCase() === "green") {
+//   swal.fire("Move Now");
+// } else {
+//   swal.fire("This color is not present on the traffic signal");
+// }
+
+// ----------------------
+
+// Q4. Write a program to take input remaining fuel in car (in litres) from user. If the current fuel is less than 0.25litres,show the message “Please refill the fuel in your car”
+
+// let remainingFuel = prompt("Enter The Remaining Fuel In Your Car (in litres)!");
+// if (remainingFuel.toLowerCase() === "0.25 litres") {
+//   swal.fire("Please refill the fuel in your car");
+// } else if (remainingFuel.toLowerCase() === "1 litre") {
+//   swal.fire("Your Fuel is about to End");
+// } else {
+//   swal.fire("You don't need to refuel");
+// }
+
+// ----------------------
+
+ 
