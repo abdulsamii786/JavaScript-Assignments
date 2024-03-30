5f4508d4a2648cc3dcfc742020275e2542851602
abdulsamii786 <154473245+abdulsamii786@users.noreply.github.com>
2024-03-30 23:22:34 +0500

    Add files via upload

---
+++ ASSIGNMENT 6/app.js
@@ -0,0 +1,34 @@
+// ==========================================
+// CHAPTER: IF...ELSE & ELSE IF STATEMENT, TESTING SET OF CONDITIONS
+// ==========================================
+
+// 1. Write a program that takes a character (number or string) in a variable & checks whether the given input is a number, uppercase letter or lower case letter. (Hint: ASCII codes:- A=65, Z=90, a=97, z=122).
+
+// ---------------------------------------------------------------------
+
+// Q2. Write a JavaScript program that accept two integers and display the larger. Also show if the two integers are equal.
+
+// let num1 = +prompt("Enter first number");
+// let num2 = +prompt("Enter second number");
+
+// if (num1 == num2) {
+//   swal.fire("Both numbers are Equal");
+// } else if (num1 < num2) {
+//   swal.fire(`The Larger Number is: ${num2}`);
+// } else if (num1 > num2) {
+//   swal.fire(`The Larger Number is: ${num1}`);
+// }
+
+// ---------------------------------------------------------------------
+
+// Q3. Write a program that takes input a number from user & state whether the number is positive, negative or zero.
+
+// let num = +prompt("Enter a number");
+
+// if (num < 0) {
+//   swal.fire(`The number ${num} is Nagative`);
+// } else if (num > 0) {
+//   swal.fire(`The number ${num} is Positive`);
+// } else if (num == 0) {
+//   swal.fire(`The number is 0`);
+// }
