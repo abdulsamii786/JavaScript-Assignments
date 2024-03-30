f47aec9aaa179b15e1be433abe329cdcf7cccdfd
abdulsamii786 <154473245+abdulsamii786@users.noreply.github.com>
2024-03-31 02:52:13 +0500

    Add files via upload

--- ASSIGNMENT 6/app.js
+++ ASSIGNMENT 6/app.js
@@ -55,3 +55,61 @@
 // } else {
 //   swal.fire("Please Enter a Single Character");
 // }
+
+// ---------------------------------------------------------------------
+
+// Q5. Write a program that
+// a. Store correct password in a JS variable.
+// b. Asks user to enter his/her password
+// c. Validate the two passwords:
+// i. Check if user has entered password. If not, then give message “ Please enter your password” ii. Check if both passwords are same. If they are same, show message “Correct! The password you entered matches the original password”. Show “Incorrect password” otherwise.
+
+// let correctPass = 987654;
+// let userPass = prompt("Enter Your Password");
+
+// if (userPass === null || userPass === "") {
+//   swal.fire("Please Enter a Password");
+// } else if (userPass === correctPass) {
+//   swal.fire("Correct!The password you entered matches the original password");
+// } else {
+//   swal.fire("Incorrect Password!");
+// }
+
+// ---------------------------------------------------------------------
+
+// Q6. This if/else statement does not work. Try to fix it:
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// else
+// greeting = "Good evening";
+// }
+
+// var greeting;
+// var hour = 18;
+// var userInput = +prompt("Enter the hour");
+// if (userInput < 18) {
+//   greeting = "Good day";
+//   swal.fire(greeting);
+// } else {
+//   greeting = "Good evening";
+//   swal.fire(greeting);
+// }
+
+// ---------------------------------------------------------------------
+
+// Q7. Write a program that takes time as input from user in 24 hours clock format like: 1900 = 7pm. Implement the following case using if, else & else if statements
+
+let time = +prompt("Enter the time in 24 hours clock format like: 1900");
+if (time >= 0o000 && time < 2359) {
+  if (time >= 0 && time < 1200) {
+    swal.fire("Good Morning");
+  } else if (time > 1200 && time < 1700) {
+    swal.fire("Good Afternoon");
+  } else if (time > 1700 && time < 2100) {
+    swal.fire("Good Afternoon");
+  } else if (time > 2100 && time < 2359) swal, fire("Good Night");
+} else {
+  swal.fire("Invalid time entered");
+}
